1bbd587179431d01923aa28cdd24248992c524ed
Micah Wylde <micah@siftscience.com>
2014-10-17 16:17:13 -0700

    JsonSchemas can have type parameters

--- jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/rules/ObjectRule.java
+++ jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/rules/ObjectRule.java
@@ -54,6 +54,7 @@ import com.sun.codemodel.JMod;
 import com.sun.codemodel.JOp;
 import com.sun.codemodel.JPackage;
 import com.sun.codemodel.JType;
+import com.sun.codemodel.JTypeVar;
 import com.sun.codemodel.JVar;
 import com.sun.codemodel.ClassType;
 
@@ -95,7 +96,7 @@ public class ObjectRule implements Rule<JPackage, JType> {
 
         JDefinedClass jclass;
         try {
-            jclass = createClass(nodeName, node, _package);
+            jclass = createClass(nodeName, node, _package, schema);
         } catch (ClassAlreadyExistsException e) {
             return e.getExistingClass();
         }
@@ -166,7 +167,10 @@ public class ObjectRule implements Rule<JPackage, JType> {
      */
     private void addConstructor(JDefinedClass jclass, JDefinedClass builderClass) {
         JMethod constructor = jclass.constructor(JMod.PRIVATE);
-        JVar builder = constructor.param(builderClass, "builder");
+
+        JVar builder = constructor.param(safeNarrow(builderClass, builderClass.typeParams()),
+                "builder");
+
         JBlock body = constructor.body();
         for (Map.Entry<String, JFieldVar> e : jclass.fields().entrySet()) {
             JExpression src = JExpr.ref(builder, e.getKey());
@@ -193,9 +197,9 @@ public class ObjectRule implements Rule<JPackage, JType> {
 
         // make sure we don't try to copy null
         return JOp.cond(
-                expr.eq(JExpr._null()),
-                JExpr._null(),
-                immutableClass.staticInvoke("copyOf").arg(expr));
+            expr.eq(JExpr._null()),
+            JExpr._null(),
+            immutableClass.staticInvoke("copyOf").arg(expr));
     }
 
     /**
@@ -206,8 +210,18 @@ public class ObjectRule implements Rule<JPackage, JType> {
 
         builderClass.constructor(JMod.PRIVATE);
 
-        JMethod buildMethod = builderClass.method(JMod.PUBLIC, jclass, "build");
-        buildMethod.body()._return(JExpr._new(jclass).arg(JExpr._this()));
+        // add type parameters if we have them
+        if (!isEmpty(jclass.typeParams())) {
+            for (JTypeVar param : jclass.typeParams()) {
+                builderClass.generify(param.name());
+            }
+        }
+
+        JMethod buildMethod = builderClass.method(JMod.PUBLIC,
+                safeNarrow(jclass, builderClass.typeParams()), "build");
+        buildMethod.body()._return(JExpr._new(
+                safeNarrow(jclass, builderClass.typeParams()))
+                .arg(JExpr._this()));
 
         return builderClass;
     }
@@ -216,17 +230,29 @@ public class ObjectRule implements Rule<JPackage, JType> {
      * Adds the newBuilder methods onto the class.
      */
     private void addNewBuilderMethods(JDefinedClass jclass, JDefinedClass builderClass) {
-        JMethod newBuilderMethod = jclass.method(JMod.PUBLIC | JMod.STATIC, builderClass, "newBuilder");
-        newBuilderMethod.body()._return(JExpr._new(builderClass));
+        JMethod newBuilderMethod =
+                jclass.method(JMod.PUBLIC | JMod.STATIC,
+                        safeNarrow(builderClass, builderClass.typeParams()), "newBuilder");
+        newBuilderMethod.body()._return(JExpr._new(
+                safeNarrow(builderClass, builderClass.typeParams())));
 
-        JMethod newBuilderFromOldMethod = jclass.method(JMod.PUBLIC | JMod.STATIC, builderClass, "newBuilder");
-        newBuilderFromOldMethod.param(jclass, "from");
+        JMethod newBuilderFromOldMethod = jclass.method(JMod.PUBLIC | JMod.STATIC,
+                safeNarrow(builderClass, builderClass.typeParams()), "newBuilder");
+        newBuilderFromOldMethod.param(safeNarrow(jclass, builderClass.typeParams()), "from");
         JBlock body = newBuilderFromOldMethod.body();
-        JVar builderVar = body.decl(builderClass, "builder", JExpr._new(builderClass));
+
+        JVar builderVar = body.decl(safeNarrow(builderClass, builderClass.typeParams()), "builder",
+                JExpr._new(safeNarrow(builderClass, builderClass.typeParams())));
         for (Map.Entry<String, JFieldVar> e : jclass.fields().entrySet()) {
             body.add(builderVar.invoke("with" + capitalize(e.getKey())).arg(
                     immutableCopy(JExpr.ref("from").ref(e.getKey()), e.getValue().type())));
         }
+
+        for (JTypeVar param : jclass.typeParams()) {
+            newBuilderMethod.generify(param.name());
+            newBuilderFromOldMethod.generify(param.name());
+        }
+
         body._return(builderVar);
     }
 
@@ -249,7 +275,8 @@ public class ObjectRule implements Rule<JPackage, JType> {
      *             that already exists, either on the classpath or in the
      *             current map of classes to be generated.
      */
-    private JDefinedClass createClass(String nodeName, JsonNode node, JPackage _package) throws ClassAlreadyExistsException {
+    private JDefinedClass createClass(String nodeName, JsonNode node, JPackage _package, Schema schema)
+            throws ClassAlreadyExistsException {
 
         JDefinedClass newType;
 
@@ -261,6 +288,15 @@ public class ObjectRule implements Rule<JPackage, JType> {
 
                 if (isPrimitive(fqn, _package.owner())) {
                     throw new ClassAlreadyExistsException(primitiveType(fqn, _package.owner()));
+                }
+
+                // determine if this is a type parameter
+                if (schema.getJavaType() instanceof JClass) {
+                    for (JTypeVar param : ((JClass) schema.getJavaType()).typeParams()) {
+                        if (fqn.equals(param.name().trim())) {
+                            throw new ClassAlreadyExistsException(param);
+                        }
+                    }
                 }
 
                 try {
@@ -276,6 +312,12 @@ public class ObjectRule implements Rule<JPackage, JType> {
                         newType = _package.owner()._class(JMod.PUBLIC, fqn, ClassType.CLASS);
                     } else {
                         newType = _package.owner()._class(fqn);
+                    }
+
+                    if (isNotEmpty(genericArguments)) {
+                        for (String parameter : genericArguments) {
+                            newType.generify(parameter.trim());
+                        }
                     }
                 }
             } else {
@@ -411,6 +453,17 @@ public class ObjectRule implements Rule<JPackage, JType> {
             return node.has("deserializationClassProperty");
         }
         return false;
+    }
+
+    /**
+     * Narrows the supplied jclass only if there are actually type parameters.
+     */
+    private JClass safeNarrow(JClass jclass, JTypeVar[] params) {
+        if (isEmpty(params)) {
+            return jclass;
+        } else {
+            return jclass.narrow(params);
+        }
     }
 
 }
